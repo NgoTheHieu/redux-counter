c3e4eb5635269cadb69fdf58786f999fb6840138
NgoTheHieu <60764766+NgoTheHieu@users.noreply.github.com>
2020-06-10 15:20:53 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Children from './components/Children';
 
 function App() {
   let count = useSelector(state => state.count)
-  let dispatch =  useDispatch()
+  let dispatch =  useDispatch() //This is dispatch
   
   
   // const increaseNum = () => {
@@ -16,16 +16,19 @@ function App() {
 
   const handleChange = (e) => {
     // e.target.value
-    dispatch({type: 'SetColor', payload: e.target.value})
+    dispatch({type: 'SetColor', payload: e.target.value}) //this handle event changes that takes user input to 
   }
 
-  
+  //  dispatch({type:"value",payload:"value"})
   return (
     <div className="App">
       <h1>Counter App</h1>
       <h2>{count}</h2>
+      {/* The format of eventHandling react-redux  dispatch({type:"value",payload:{what value you want to put in here}}) */}
       <button onClick={() => dispatch({type:'Increment', payload: 1})}>Increment</button>
+      {/* Everytime you click on Increment React-redux dispatch will dispatch back to its router machine, like that ..*/}
       {count == 0 ? <button disabled='true'>Disabled</button> : <button onClick={() => dispatch({type:'Decrement', payload: 1})}>Decrement</button>}
+
       <button onClick={() => dispatch({type:'Reset'})}>Reset</button>
       <input onChange={handleChange} placeholder='change color'></input>
       <Children/>
--- src/components/Children.js
+++ src/components/Children.js
@@ -5,27 +5,28 @@ import Box from '../Box'
 export default function Children() {
     let count = useSelector(state => state.count)
     let color = useSelector(state => state.color)
-    let [boxColor, setBoxColor] = React.useState(null)
+    let [boxColor, setBoxColor] = React.useState(null)   //
    
    
     console.log("color is: ", color)
-    let dispatch = useDispatch()
-    let textColor = "white"
+    let dispatch = useDispatch()    //let dispatch = useDispatch()
+    let textColor = "white"     //Initially the color of the box is white
 
    
     if (count > 10) {
-        textColor = "yellow"
+        textColor = "yellow"    //Automatically turn the whole lines of boxes into yellow if ...
     }
     return (
+        // This is just how You would make your array listed as boxes
         <div>
-            {Array(count).fill('box').map(box => {
+            {Array(count).fill('box').map(box => {      // This is how you learn to render an array to your field
                return(
-                   <Box></Box>
+                   <Box></Box>                          //  Yaaaaaaah
                )
-
 
 
             })}
         </div>
+        // This is 
     )
 }
